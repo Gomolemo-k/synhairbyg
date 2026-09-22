9b1de8391f865c2f399c49b076e8fc2ebeb65917
Gomolemo Kgatitsoe <kgatitsoeg321@gmail.com>
2026-09-22 18:11:19 +0200

    fixed errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      data-scroll-behavior="smooth"
       className={`${geistSans.variable} ${geistMono.variable} ${playfairDisplay.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">
